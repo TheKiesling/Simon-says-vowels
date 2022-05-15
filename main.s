10983ca659f39cea0ccea2aa7376672dde9fb9fb
adrianRFlores <84111818+adrianRFlores@users.noreply.github.com>
2022-05-14 22:06:16 -0600

    Documentacion

--- main.s
+++ main.s
@@ -25,7 +25,6 @@ main:
 	
 	mov 	r5, #7					// Indicar cantidad de pins
 	mov 	r6,#5					// Indicar la cantidad de vocales
-	mov 	r7,#1
 
 	bl		wiringPiSetupPhys		// Inicializar librería wiringpi
 	mov		r1,#-1					// -1 representa un código de error
@@ -44,65 +43,65 @@ config:
 	subs 	r5,#1					// resta 1 y lo guarda en bandera
 	bne 	config 					// regresar a etiqueta
 
-	mov 	r9,#0
+	mov 	r9,#0 					// Iguala r9 a 0 para iterar sobre el array
 	mov 	r5,#7
 
 reset:
-	ldr 	r4,=pin
-	ldr 	r0,[r4,r9]
-	mov 	r1,#0
-	bl 	    digitalWrite			
-	add 	r9,#4
-	subs 	r5,#1						
+	ldr 	r4,=pin 				// carga el array de pines a r4
+	ldr 	r0,[r4,r9]				// Accede al elemento con indice = r9 y lo carga a r0
+	mov 	r1,#0 					// r1 = 0 para apagar el segmento conectado al pin
+	bl 	    digitalWrite			// Apaga el valor del pin cargado en r0
+	add 	r9,#4					// Agrega 4 a r9 para cambiar de posicion en el array
+	subs 	r5,#1					// resta 1 a r5 y lo compara con 0 para determinar si ya se iteró sobre el array	
 	bne 	reset 			
 
 print_vowels:
-	cmp 	r6,#5
-	ldreq 	r4,=a
-	moveq 	r5,#6
+	cmp 	r6,#5 					// Revisa el valor de r6 para poder cargar la vocal adecuada 
+	ldreq 	r4,=a                   // 5 = a
+	moveq 	r5,#6     				// Carga el tamaño del array de la vocal para iterar
 
-	cmp 	r6,#4
-	ldreq 	r4,=e
-	moveq 	r5,#5
+	cmp 	r6,#4         			// Revisa el valor de r6 para poder cargar la vocal adecuada 
+	ldreq 	r4,=e   				// 4 = e
+	moveq 	r5,#5 					// Carga el tamaño del array de la vocal para iterar
 
-	cmp 	r6,#3
-	ldreq 	r4,=i
-	moveq 	r5,#2
+	cmp 	r6,#3 					// Revisa el valor de r6 para poder cargar la vocal adecuada 
+	ldreq 	r4,=i 					// 3 = i
+	moveq 	r5,#2 					// Carga el tamaño del array de la vocal para iterar
 
-	cmp 	r6,#2
-	ldreq 	r4,=o
-	moveq 	r5,#6
+	cmp 	r6,#2 					// Revisa el valor de r6 para poder cargar la vocal adecuada 
+	ldreq 	r4,=o 					// 2 = o
+	moveq 	r5,#6  					// Carga el tamaño del array de la vocal para iterar
 
-	cmp 	r6,#1
-	ldreq 	r4,=u
-	moveq 	r5,#5
+	cmp 	r6,#1 					// Revisa el valor de r6 para poder cargar la vocal adecuada
+	ldreq 	r4,=u 					// 1 = u
+	moveq 	r5,#5 					// Carga el tamaño del array de la vocal para iterar
 
-	mov 	r9,#0
+	mov 	r9,#0 					// Se resetea el valor de r9 para iterar en el loop
 
 	change_display:
-		ldr 	r0,[r4,r9]
-		mov 	r1,#1
-		bl 	    digitalWrite			
-		add 	r9,#4
-		subs 	r5,#1					
+		ldr 	r0,[r4,r9] 			// Se carga el elemento del array con indice = r9 a r0
+		mov 	r1,#1 				// r1 = 1 para encender el segmento conectado al pin
+		bl 	    digitalWrite		// Se llama la funcion para actualizar el pin cargado en r0	
+		add 	r9,#4 				// Agrega 4 a r9 para cambiar de posicion en el array
+		subs 	r5,#1				// resta 1 a r5 y lo compara con 0 para determinar si ya se iteró sobre el array	
 		bne 	change_display 			
 
-	ldr		r0,=delayMs
+	ldr		r0,=delayMs 			// Se carga el valor del delay en la etiqueta
 	ldr		r0,[r0]
-	bl		delay 
+	bl		delay  					// Se realiza el delay para poder visualizar el output
 
-	mov		r9,#0
-	mov 	r5,#7
-	subs 	r6,#1
+	mov		r9,#0 					
+	mov 	r5,#7 					// Se cargan los valores necesarios en r9 y r5 para poder realizar el reset
+	subs 	r6,#1 				    // Si r6 != 0, se hace un reset. De lo contrario, se continua a final_reset
 	bne 	reset
 
 final_reset:
-	ldr 	r4,=pin
-	ldr 	r0,[r4,r9]
-	mov 	r1,#0
-	bl 	    digitalWrite			
-	add 	r9,#4
-	subs 	r5,#1					
+	ldr 	r4,=pin 				// Se carga el array en la etiqueta pin
+	ldr 	r0,[r4,r9] 				// Se carga el elemento del array con indice = r9 a r0
+	mov 	r1,#0 					// r1 = 0 para apagar el segmento conectado al pin
+	bl 	    digitalWrite			// Se llama la funcion para actualizar el pin cargado en r0
+	add 	r9,#4 					// Agrega 4 a r9 para cambiar de posicion en el array
+	subs 	r5,#1					// resta 1 a r5 y lo compara con 0 para determinar si ya se iteró sobre el array
 	bne 	final_reset 			
 
 exit:
