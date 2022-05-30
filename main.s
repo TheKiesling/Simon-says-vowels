1d6affae29c70f769b1fbc3018cfd5e45c16b3cd
TheKiesling <kie21581@uvg.edu.gt>
2022-05-29 21:45:49 -0600

    final version

--- main.s
+++ main.s
@@ -22,6 +22,9 @@
 
 main:   
 	push 	{ip, lr}	
+
+	ldr		r0,=bienvenida			// Mostrar mensaje de bienvenida
+	bl 		puts
 	
 	mov 	r5, #9					// Indicar cantidad de pins
 
@@ -75,15 +78,22 @@ menu:
 	bl 		puts
 	ldr 	r0, =formatos			// Leer opcion del usuario
 	ldr 	r1, =op
-	bl 		scanf			
+	bl 		scanf	
+
+	cmp r0,#0						// Validar si el ingreso es valido
+	beq Error		
 	
 	ldr 	r1, =op					// Cargar opcion a realizar
 	ldrb 	r1, [r1]
 	cmp 	r1, #113				// Si es q (113 - ASCII) sale del programa
 	beq 	exit
 
+	cmp 	r1, #120				// Si no es x (120 - ASCII) ha ingresado una opcion no valida
+	bne		ErrorMenu
+
 	mov 	r9,#0 					// Iguala r9 a 0 para iterar sobre el array
 	mov 	r5,#7					// Indicar cantidad de pins
+
 
 reset:
 	ldr 	r4,=pinOutput			// carga el array de pines a r4
@@ -113,21 +123,17 @@ print_vowels:
 	bl 		_rnd					// llama a la subrutina 
 
 	ldr 	r8,=vowels				// cargar el arreglo de vocales
-	mov 	r11,r0						// obtener vocal generada aleatoriamente
+	mov 	r11,r0					// obtener vocal generada aleatoriamente
 	strb	r11,[r8,r7]            	// guarda en vocales la vocal generada en la posicion que corresponde por r7
 
 	cmp 	r11,#'a'
 	ldreq 	r4,=a                   // cargar el arreglo con los pines para generar 'a'
-
 	cmp 	r11,#'e'
 	ldreq 	r4,=e                   // cargar el arreglo con los pines para generar 'e'
-
 	cmp 	r11,#'i'
 	ldreq 	r4,=i                   // cargar el arreglo con los pines para generar 'i'
-
 	cmp 	r11,#'o'
 	ldreq 	r4,=o                  	// cargar el arreglo con los pines para generar 'o'
-
 	cmp 	r11,#'u'
 	ldreq 	r4,=u                   // cargar el arreglo con los pines para generar 'u'
 
@@ -175,6 +181,9 @@ reset2:
 	ldr 	r0, =formatos			// leer cadena de caracteres que el usuario cree correctas
 	ldr 	r1, =vowels2
 	bl 		scanf
+
+	cmp		r0,#0					// Validar si el ingreso es valido
+	beq 	Error
 	
 entrada:
 	ldr 	r8, =vowels				// carga arreglo de vocales
@@ -281,7 +290,21 @@ try2:
 		bne 	reset_array			// revisar si aun quedan elementos en el arreglo a resetear
 		b 		menu				// ya permite presionar el boton
 
-exit:
+Error:
+	ldr r0,=ErrInput				// Imprimir mensaje de error de ingreso
+	bl puts
+
+ErrorMenu:
+	ldr r0,=ErrMenu					// Imprimir mensaje de error de menu
+    bl puts
+	ldr		r0,=delayMs 			// Se carga el valor del delay en la etiqueta
+	ldr		r0,[r0]
+	bl		delay  					// Se realiza el delay para poder visualizar el output
+    b menu
+
+exit:	
+	ldr 	r0,=salida				// Mostrar mensaje de despedida
+	bl puts
 	pop 	{ip, pc}
 
 /* --------------------------------------- DATA --------------------------------------- */
@@ -289,25 +312,27 @@ exit:
 .balign 4
 
 /* --- Variables --- */
-a: 			.word 	29,31,33,36,37,38,0
-e: 			.word 	29,35,36,37,38,0
-i: 			.word 	36,37,0
-o: 			.word 	29,31,33,35,36,37,0
-u: 			.word 	31,33,35,36,37,0
-pinOutput:	.word 	29,31,33,35,36,37,38,13,15
-pinInput: 	.word 	16,18
-pb:			.word	0
-rec:		.word	0
-op:			.asciz	" "
-delayMs:	.int	1000
-delay2:		.int	100
-OUTPUT		=		1	
+a: 			.word 	29,31,33,36,37,38,0 			// Pines para generar A
+e: 			.word 	29,35,36,37,38,0				// Pines para generar E
+i: 			.word 	36,37,0							// Pines para generar I
+o: 			.word 	29,31,33,35,36,37,0				// Pines para generar O
+u: 			.word 	31,33,35,36,37,0				// Pines para generar U
+pinOutput:	.word 	29,31,33,35,36,37,38,13,15		// Pines de salida
+pinInput: 	.word 	16,18							// Pines de entrada
+pb:			.word	0								// Puntuacion maxima
+rec:		.word	0								// Puntuacion minima
+op:			.asciz	" "								// Opcion a realizar
+delayMs:	.int	1000							// Delay de 1 seg
+delay2:		.int	100								// Delay de 0.1 seg
+OUTPUT		=		1								
 INPUT		=		0
-vowels:		.asciz  "             	 "
-vowels2:	.asciz  "             	 "
+vowels:		.asciz  "             	 "				// Arreglo de vocales generadas random
+vowels2:	.asciz  "             	 "				// Secuencia de vocales ingresadas por el usuario
 
 /* --- Mensajes --- */
 ErrMsg:	 	.asciz	"Setup didn't work... Aborting...\n"
+ErrInput:	.asciz  "Asegurese de ingresar caracteres."
+ErrMenu: 	.asciz 	"Ingrese una opcion valida"
 resetMsg:	.asciz	"\nPerdiste :(\n"
 winMsg:	 	.asciz	"¡Ganaste!\n"
 formatod: 	.asciz "%d"
@@ -321,3 +346,5 @@ cont2:		.asciz "Activa el sensor para regresar al menu... "
 maxPts:		.asciz "Puntuacion mas alta: %d\n"
 reciente:	.asciz "Intento anterior: %d\n"
 menuMsg: 	.asciz "\n--------------MENU--------------\nx) Jugar :D\nq) Salir\n\nIngrese una opcion: "
+bienvenida:	.asciz "Hola! Bienvenido a Simon Dice de vocales. \nSe presentara en un display una secuencia de letras y deberas de ingresarla \n Buena suerte"
+salida:		.asciz "Hasta la proxima"
